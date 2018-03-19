3f2f866036a72131c3819342a6a6b9db59f4e2a8
Livia <liviaishak1@gmail.com>
2018-03-18 18:58:54 -0700

    cp18: completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,24 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
 
   def show
     @post = Post.find(params[:id])
   end
 
   def new
-    @post = Post.new
+    @topic = Topic.find(params[:topic_id])
   end
 
   def create
     @post = Post.new
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
+    @topic = Topic.find(params[:topic_id])
+
+    @post.topic = @topic
 
     if @post.save
       flash[:notice] = "Post was saved."
-      redirect_to @post
+      redirect_to [@topic, @post]
     else
       flash.now[:alert] = "There was an error saving the post. Please try again."
       render :new
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
 
     if @post.save
       flash[:notice] = "Post was updated."
-      redirect_to @post
+      redirect_to [@post.topic, @post]
     else
       flash.now[:alert] = "There was an error saving the post. Please try again."
       render :edit
@@ -48,10 +48,10 @@ class PostsController < ApplicationController
 
     if @post.destroy
       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-      redirect_to posts_path
+      redirect_to @post.topic
     else
       flash.now[:alert] = "There was an error deleting the post."
       render :show
     end
-  end 
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,33 @@
 require 'random_data'
 
+#Create Topics
+15.times do
+  Topic.create!(
+    name: RandomData.random_sentence,
+    description: RandomData.random_paragraph
+  )
+end
+  topics = Topic.all
+
+#Create Posts
 50.times do
-
   Post.create!(
-
+    topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
 end
 posts = Post.all
 
+#Create Comments
 100.times do
   Comment.create!(
-
     post: posts.sample,
     body: RandomData.random_paragraph
   )
 end
 
-puts "#{Post.count}"
-post_unique = Post.find_or_create_by(
-                title: "A unique title",
-                body: "A unique body"
-              )
-puts "#{Post.count}"
-
-puts "#{Comment.count}"
-Comment.find_or_create_by(
-  post: post_unique,
-  body: "A unique comment body"
-)
-puts "#{Comment.count}"
-
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
