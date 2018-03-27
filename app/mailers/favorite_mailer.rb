ba80818320d05d7f9ffda2cae7ad87f2a7809189
Livia <liviaishak1@gmail.com>
2018-03-26 20:43:02 -0700

    assignment27: completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -1,5 +1,5 @@
 class FavoriteMailer < ApplicationMailer
-  default from: "youremail@email.com"
+  default from: "liviaishak1@gmail.com"
 
 
    def new_comment(user, post, comment)
@@ -14,4 +14,17 @@ class FavoriteMailer < ApplicationMailer
 
      mail(to: user.email, subject: "New comment on #{post.title}")
    end
+
+   def new_post(post)
+
+     headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
+     headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+     headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+     @post = post
+
+     mail(to: post.user.email, subject: "You're following, #{post.title}!")
+   end
+
+
 end
