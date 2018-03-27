560b7634a73f08c5c3090c6c2d92cd800871a105
Livia <liviaishak1@gmail.com>
2018-03-26 23:28:22 -0700

    assignment28: completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorite_posts, through: :favorites, source: :post
 
   before_save { self.email = email.downcase if email.present? }
   before_save { self.role ||= :member }
@@ -19,7 +20,7 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  enum role: [:member, :admin, :moderator]
+  enum role: [:member, :admin]
 
   def favorite_for(post)
      favorites.where(post_id: post.id).first
